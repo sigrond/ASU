1884cbf74d6490d5cae2fb4a725235b953f60148
Tomasz Jakubczyk <sigrond93@gmail.com>
2016-10-23 15:12:02 +0200

    sprawdzanie wysokości i szerokości tabelki i odpowiednie jej dopasowanie, wczytanie wszystkich wartości do macierzy

--- projekt1.pl
+++ projekt1.pl
@@ -2,16 +2,51 @@
 
 #print "Hello World!\n";
 
+use strict;
+#use warnings;
+
+use List::Util qw[min max];
+use Getopt::Std;
+
 open(DATA,"<test.txt") or die "Couldn't open file test.txt, $!";
-@lines=<DATA>;
+my @lines=<DATA>;
 close(DATA);
-print '\documentclass{article}'."\n";
-print '\begin{document}'."\n";
-print '\begin{tabular}{|c|c|c|}';
+
+my @matrix;
+my $width=0;
+my $height=0;
+
 foreach(@lines)
 {
+	my @line=split(/ /,$_);
+	push @{ $matrix[$height] }, @line;
+	$width=max($width,scalar @line);
+	$height++;
+}
+
+#print "width: $width, height: $height\n";
+
+
+
+print '\documentclass{article}'."\n";
+print '\begin{document}'."\n";
+print '\begin{tabular}{|';
+for(my $i=0;$i<$width;$i++)
+{
+	print "c|";
+}
+print '}';
+for(my $i=0;$i<$height;$i++)
+{
 	print '\hline'."\n";
-	print join(' & ',split(/ /,$_));
+	for(my $j=0;$j<$width;$j++)
+	{
+		print $matrix[$i]->[$j];
+		if($j<($width-1))
+		{
+			print " & ";
+		}
+	}
 	print "\\\\\n";
 }
 print '\hline'."\n";
