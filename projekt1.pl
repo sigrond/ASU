f0b0cb92851c5ef6f924c5c201952e3071a74e35
Tomasz Jakubczyk <sigrond93@gmail.com>
2016-10-23 19:19:24 +0200

    przełącznik transpozycji i jej wykonanie

--- projekt1.pl
+++ projekt1.pl
@@ -6,7 +6,7 @@ use strict;
 #use warnings;
 
 use List::Util qw[min max];
-use Getopt::Std;
+use Getopt::Long;
 
 open(DATA,"<test.txt") or die "Couldn't open file test.txt, $!";
 my @lines=<DATA>;
@@ -26,7 +26,15 @@ foreach(@lines)
 
 #print "width: $width, height: $height\n";
 
+my $transpose=0;
+GetOptions('transpose' => \$transpose) or die "Usage: $0 --transpose\n";
 
+#print $transpose."\n";
+if($transpose)
+{
+	#print "transpozycja\n";
+	($width, $height)=($height, $width);
+}
 
 print '\documentclass{article}'."\n";
 print '\begin{document}'."\n";
@@ -41,7 +49,14 @@ for(my $i=0;$i<$height;$i++)
 	print '\hline'."\n";
 	for(my $j=0;$j<$width;$j++)
 	{
-		print $matrix[$i]->[$j];
+		if($transpose)
+		{
+			print $matrix[$j]->[$i];
+		}
+		else
+		{
+			print $matrix[$i]->[$j];
+		}
 		if($j<($width-1))
 		{
 			print " & ";
